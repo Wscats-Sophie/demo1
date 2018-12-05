4520feb720c476348501c822a118fb85f17040b5
sophie <1063432393@qq.com>
2018-12-05 16:13:45 +0800

    111111

--- src/com/company/Persom.java
+++ src/com/company/Persom.java
@@ -2,9 +2,12 @@ package com.company;
 
 public class Persom {
     public void pu(){
+        System.out.println("pu7方法");
         System.out.println("pu方法");
         System.out.println("pu2方法");
         System.out.println("pu3方法");
         System.out.println("pu4方法");
+        System.out.println("pu6方法");
+
     }
 }
